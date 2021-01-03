c9cb5b712e62a89fa633b4b4387d37f6ce6e2e1c
ArkjuniorK <created.arkjuniork@gmail.com>
2021-01-03 09:10:36 +0800

    Recolor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.vue'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': '1440px'
+      },
+      height: {
+        'screen-4/5': '80vh'
+      }
+    },
     fontFamily: {
       lato: 'Lato, sans-serif',
       raleway: 'Raleway, sans-serif'
@@ -10,10 +17,13 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       light: '#fff',
-      red: {
-        50: '#FFF8F2',
-        100: '#FF8282',
-        200: '#6F2323'
+      dark: '#080808',
+      blue: {
+        0: '#D4F1F4',
+        50: '#7EC8E3',
+        100: '#0000FF',
+        200: '#000C66',
+        300: '#050A30'
       }
     }
   },
